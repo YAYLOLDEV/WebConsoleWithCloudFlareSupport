539a55045f8cbf2dfc9d559a9c7ef5d60cd7f0f7
LOLYAY <lolyay@lolyay.de>
2025-02-28 22:52:32 +0100

    commit

--- scripts/WebConsole.js
+++ scripts/WebConsole.js
@@ -2,8 +2,8 @@
  Main JS file for WebConsole.
  https://github.com/mesacarlos
  2019-2020 Carlos Mesa under MIT License.
-*/
-var mychart = null
+ */
+
 /**
  * Global variables
  */
@@ -19,29 +19,47 @@ let commandHistoryIndex = -1; //Saves current command history index. -1 when not
  * and auto update in next request when file is changed
  */
 function readServerList() {
-	console.log("Servers.json Not implemented");
+	let hash = persistenceManager.getSetting('server:hash')
+
+	/**
+	 * Hash code function used for compare version of file servers.json
+	 * https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+	 */
+	const hashCode = s => s.split('').reduce((a,b)=>{a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
+
+	fetch('servers.json')
+		.then(res => res.text())
+		.then(json => {
+			if (hash !== hashCode(json)) {
+				persistenceManager.setSetting('server:hash', hashCode(json))
+				JSON.parse(json).forEach(server => persistenceManager.saveServer(server))
+			}
+		})
+		.then(updateServerList)
+		.catch(() => console.info('Ignore load new list in file servers.json.'));
 }
 
-var curserver = null
-	function openServer(serverName){
-	curserver = serverName
+/**
+ * Prepare and show server to user
+ */
+function openServer(serverName){
 	//Hide welcome div if user is not in welcome page
 	$("#welcomeContainer").hide();
 	$("#serverContainer").show();
-	
+
 	//Change server name and related info
 	$("#serverTitle").text(serverName);
 	$("#consoleTextArea").text("");
 	$("#commandInput").prop("disabled", false);
 	$("#sendCommandButton").prop("disabled", false);
-	
+
 	//New server, new variables:
 	autoPasswordCompleted = false;
 	commandHistoryIndex = -1; //Reset command history index
-	
+
 	//Create or retrieve connection
 	connectionManager.loadConnection(serverName);
-	
+
 	//Load saved messages
 	let i;
 	const messages = connectionManager.activeConnection.messages;
@@ -50,13 +68,12 @@ var curserver = null
 			onWebSocketsMessage(messages[i]);
 		}
 	}
-	
+
 	//Subscribe a function
 	connectionManager.activeConnection.subscribe(onWebSocketsMessage);
 }
 
 function onWebSocketsMessage(message){
-
 	switch (message.status) {
 		case 10:
 			//Console Output
@@ -77,9 +94,12 @@ function onWebSocketsMessage(message){
 			}
 
 			//Read log file if enabled
+			if(connectionManager.activeConnection.isLogged === false){
+				connectionManager.activeConnection.isLogged = true;
+				if(persistenceManager.getSetting("retrieveLogFile") === true)
+					connectionManager.askForLogs();
+			}
 			break;
-		case 69:
-			writeToWebConsole("Token Authenticated!");
 		case 400:
 			//Unknown Command
 			writeToWebConsole(message.message);
@@ -108,111 +128,50 @@ function onWebSocketsMessage(message){
 			writeRamInfo(message.free, message.used, message.max);
 			break;
 		case 1003:
-			if(message.hasOwnProperty("tps")){
-				writeTpsInfo(Math.round(message.tps), 20);
-				break;
-			}
-			const ctx = document.getElementById('chart').getContext('2d');
-			const data = message.tpsh;
-			const pdata = message.ph;
-    // Create the chart
-    myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: Array.from({ length: Object.values(data).length}, (_, i) => `Hour ${i + 1}`),
-        datasets: [{
-          label: 'Tps',
-          data: Object.values(data),
-          backgroundColor: 'rgba(255, 255, 255, 1)',
-          borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1
-        },{
-			label: 'Players',
-			data: Object.values(pdata),
-			backgroundColor: 'rgba(255, 255, 255, 1)',
-			borderColor: 'rgba(99, 255, 164, 1)',
-			borderWidth: 1
-		  }]
-      },
-      options: {
-        scales: {
-          x: {
-            title: {
-              display: true,
-              text: 'Hour'
-            }
-          },
-          y: {
-            title: {
-              display: true,
-              text: 'Count'
-            }
-          }
-        }
-      }
-    });
+			//Server TPS
+			writeTpsInfo(message.tps, 20);
 			break;
 		default:
 			console.log('Unknown server response:');
 	}
+	console.log(message);
 
 	//Add interval for Players, CPU and RAM info, if not set
 	if(statusCommandsInterval === -1 && message.status !== 401){
 		statusCommandsInterval = setInterval(function(){
 			connectionManager.askForInfo();
-		}, 1000);
+		}, 2500);
 	}
 }
-$('#tpsmodal').on('hidden.bs.modal', function (e) {
-	// do something...
-	myChart.destroy()
-})
-
-function tpsclick(){
-	connectionManager.test()
-
-}
-
 
 /**
-* Write to console
-*/
+ * Write to console
+ */
 function writeToWebConsole(msg, time){
-
 	const isScrolledDown = document.getElementById("consoleTextArea").scrollHeight - document.getElementById("consoleTextArea").scrollTop - 40 === $("#consoleTextArea").height();
 
 	//Write to div, replacing < to &lt; (to avoid XSS) and replacing new line to br.
 	msg = msg.replace(/</g, "&lt;");
 	msg = msg.replace(/(?:\r\n|\r|\n)/g, "<br>");
-	
+
 	//Color filter for Windows (thanks to SuperPykkon)
-	msg = msg.replace(/\[38;5;0m/g, "<span style='color: #000000;'>"); //&0 //fixed
-	msg = msg.replace(/\[38;5;4m/g, "<span style='color: #0000AA;'>"); //&1 //fixed
-	msg = msg.replace(/\[38;5;2m/g, "<span style='color: #00AA00;'>"); //&2 //fixed
-	msg = msg.replace(/\[38;5;6m/g, "<span style='color: #00AAAA;'>"); //&3 //fixed
-	msg = msg.replace(/\[38;5;9m/g, "<span style='color: #AA0000;'>"); //&4 //fixed
-	msg = msg.replace(/\[38;5;1m/g, "<span style='color: #AA0000;'>"); //&4 //fixed
-	msg = msg.replace(/\[38;5;5m/g, "<span style='color: #AA00AA;'>"); //&5 //fixed
-	msg = msg.replace(/\[38;5;3m/g, "<span style='color: #FFAA00;'>"); //&6 //fixed
-	msg = msg.replace(/\[0;37;22m/g, "<span style='color: #AAAAAA;'>"); //&7 //fixed
-	msg = msg.replace(/\[38;5;8m/g, "<span style='color: #555555;'>");  //&8 //fixed
-	msg = msg.replace(/\[38;5;12m/g, "<span style='color: #5555FF;'>");  //&9 //fixed
-	msg = msg.replace(/\[38;5;10m/g, "<span style='color: #55FF55;'>");  //&a //fixed
-	msg = msg.replace(/\[38;5;14m/g, "<span style='color: #55FFFF;'>");  //&b //fixed
+	msg = msg.replace(/\[0;30;22m/g, "<span style='color: #000000;'>"); //&0
+	msg = msg.replace(/\[0;34;22m/g, "<span style='color: #0000AA;'>"); //&1
+	msg = msg.replace(/\[0;32;22m/g, "<span style='color: #00AA00;'>"); //&2
+	msg = msg.replace(/\[0;36;22m/g, "<span style='color: #00AAAA;'>"); //&3
+	msg = msg.replace(/\[0;31;22m/g, "<span style='color: #AA0000;'>"); //&4
+	msg = msg.replace(/\[0;35;22m/g, "<span style='color: #AA00AA;'>"); //&5
+	msg = msg.replace(/\[0;33;22m/g, "<span style='color: #FFAA00;'>"); //&6
+	msg = msg.replace(/\[0;37;22m/g, "<span style='color: #AAAAAA;'>"); //&7
+	msg = msg.replace(/\[0;30;1m/g, "<span style='color: #555555;'>");  //&8
+	msg = msg.replace(/\[0;34;1m/g, "<span style='color: #5555FF;'>");  //&9
+	msg = msg.replace(/\[0;32;1m/g, "<span style='color: #55FF55;'>");  //&a
+	msg = msg.replace(/\[0;36;1m/g, "<span style='color: #55FFFF;'>");  //&b
 	msg = msg.replace(/\[0;31;1m/g, "<span style='color: #FF5555;'>");  //&c
-	msg = msg.replace(/\[38;5;13m/g, "<span style='color: #FF55FF;'>");  //&d //fixed
-	msg = msg.replace(/\[38;5;11m/g, "<span style='color: #FFFF55;'>");  //&e  //fixed
-	msg = msg.replace(/\[38;5;15m/g, "<span style='color: #FFFFFF;'>");  //&f //fixed
-	msg = msg.replace(/\[0m/g, "</span>");  //&f                                      //fixed
-	msg = msg.replace(/\[4m/g, "<span style='text-decoration: underline;'>");  //added
-	msg = msg.replace(/\[38;5;7m/g, "<span style='color: #FFFFFF;'>");  //added bold
-	msg = msg.replace(/\[3m/g, "</span></span><span style='#FF55FF;'>");  //added  background-color: red;
-	
-	//TODO
-	//BOLD: [38;5;7m
-	
-
-
+	msg = msg.replace(/\[0;35;1m/g, "<span style='color: #FF55FF;'>");  //&d
+	msg = msg.replace(/\[0;33;1m/g, "<span style='color: #FFFF55;'>");  //&e
+	msg = msg.replace(/\[0;37;1m/g, "<span style='color: #FFFFFF;'>");  //&f
+	msg = msg.replace(/\[m/g, "</span>");  //&f
 
 	//Color filter for UNIX (This is easier!)
 	//span may not be closed every time but browsers will do for ourselves
@@ -232,12 +191,12 @@ function writeToWebConsole(msg, time){
 	msg = msg.replace(/§d/g, "<span style='color: #FF55FF;'>"); //&d
 	msg = msg.replace(/§e/g, "<span style='color: #FFFF55;'>"); //&e
 	msg = msg.replace(/§f/g, "<span style='color: #FFFFFF;'>"); //&f
-	
+
 	msg = msg.replace(/§l/g, "<span style='font-weight:bold;'>"); //&l
 	msg = msg.replace(/§m/g, "<span style='text-decoration: line-through;'>"); //&m
 	msg = msg.replace(/§n/g, "<span style='text-decoration: underline;'>"); //&n
 	msg = msg.replace(/§o/g, "<span style='font-style: italic;'>"); //&o
-	
+
 	msg = msg.replace(/§r/g, "</span>");  //&r
 
 	//Append datetime if enabled
@@ -249,10 +208,10 @@ function writeToWebConsole(msg, time){
 		else
 			msg = "[" + new Date().toLocaleTimeString() + "] " + msg;
 	}
-		
-	
+
+
 	$("#consoleTextArea").append(msg + "<br>");
-	
+
 	if(isScrolledDown){
 		const textarea = document.getElementById('consoleTextArea');
 		textarea.scrollTop = textarea.scrollHeight;
@@ -260,8 +219,8 @@ function writeToWebConsole(msg, time){
 }
 
 /**
-* Fill connected players card
-*/
+ * Fill connected players card
+ */
 function writePlayerInfo(connected, maximum){
 	$("#connectedPlayers").text(connected);
 	$("#maxPlayers").text(maximum);
@@ -271,19 +230,18 @@ function writePlayerInfo(connected, maximum){
 }
 
 /**
-* Fill CPU info card
-*/
+ * Fill CPU info card
+ */
 function writeCpuInfo(usage){
 	$("#cpuInfo").text(usage + "%");
-	
+
 	$("#CpuProgressBar").width(usage + "%");
 }
 
 /**
-* Fill RAM info card
-*/
+ * Fill RAM info card
+ */
 function writeRamInfo(free, used, total){
-	$('#loadingmodal').modal('hide');
 	$("#usedRam").text(used);
 	$("#totalRam").text(total);
 
@@ -295,8 +253,8 @@ function writeRamInfo(free, used, total){
  * Fill TPS info card
  */
 function writeTpsInfo(tps, max){
-	if(tps > max) {
-		tps = max;
+	if(tps > 20) {
+		tps = 20;
 	}
 	$("#tps").text(tps);
 	$("#maxTps").text(max);
@@ -305,9 +263,45 @@ function writeTpsInfo(tps, max){
 	$("#TpsProgressBar").width(percent + "%");
 }
 
+
 /**
-* Called from WebConsoleConnector only.
-*/
+ * Shows welcome screen
+ */
+function backToHomepage(){
+	//Stop gathering info from server
+	clearInterval(statusCommandsInterval);
+	statusCommandsInterval = -1;
+
+	//Reset command history index
+	commandHistoryIndex = -1;
+
+	//Clear all server indicators
+	writePlayerInfo(0, 0);
+	writeCpuInfo(0);
+	writeRamInfo(0, 0, 0);
+
+	$("#welcomeContainer").show();
+	$("#serverContainer").hide();
+}
+
+/**
+ * Update dropdown with saved server list
+ */
+function updateServerList(){
+	//Delete all servers in dropdown
+	$('.servermenuitem').remove();
+
+	//Add all servers
+	const servers = persistenceManager.getAllServers();
+	for(let i = 0; i < servers.length; i++){
+		$('#ServerListDropDown').append('<a class="dropdown-item servermenuitem" href="#" onclick="openServer(\'' + servers[i].serverName + '\')">' + servers[i].serverName.replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/'/g,"").replace(/"/g,"") + '</a>');
+	}
+
+	//Show a "no servers" message when no servers are added
+	if(servers.length === 0){
+		$('#ServerListDropDown').append('<a class="dropdown-item servermenuitem disabled" href="#" id="noServersAdded">No servers added</a>');
+	}
+}
 function closedConnection(serverName){
 	if(connectionManager.activeConnection.serverName === serverName){
 		//Disable command input and button
@@ -321,53 +315,3 @@ function closedConnection(serverName){
 	connectionManager.deleteConnection(serverName, true);
 }
 function retry(){openServer(connectionManager.activeConnection.serverName);}
-function rsServer(){
-	connectionManager.sendConsoleCmd("restart");
-
-}
-
-/**
-* Shows welcome screen
-*/
-function backToHomepage(){
-	//Stop gathering info from server
-	clearInterval(statusCommandsInterval);
-	statusCommandsInterval = -1;
-
-	//Reset command history index
-	commandHistoryIndex = -1;
-	
-	//Clear all server indicators
-	writePlayerInfo(0, 0);
-	writeCpuInfo(0);
-	writeRamInfo(0, 0, 0);
-	
-	$("#welcomeContainer").show();
-	$("#serverContainer").hide();
-}
-
-function dev_opt(){
-	data = $('#datanew')
-	decodeddata = atob(data.val())
-	localStorage.setItem('WebConsole', decodeddata)
-
-}
-
-/**
-* Update dropdown with saved server list
-*/
-function updateServerList(){
-	//Delete all servers in dropdown
-	$('.servermenuitem').remove();
-	
-	//Add all servers
-	const servers = persistenceManager.getAllServers();
-	for(let i = 0; i < servers.length; i++){
-		$('#ServerListDropDown').append('<a class="dropdown-item servermenuitem" href="#" onclick="openServer(\'' + servers[i].serverName + '\')">' + servers[i].serverName.replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/'/g,"").replace(/"/g,"") + '</a>');
-	}
-	
-	//Show a "no servers" message when no servers are added
-	if(servers.length === 0){
-		$('#ServerListDropDown').append('<a class="dropdown-item servermenuitem disabled" href="#" id="noServersAdded">No servers added</a>');
-	}
-}
--- scripts/login.js
+++
@@ -1,5 +0,0 @@
-function loginbtnclicked(){
-    console.log("oh suck my dick btw")
-
-
-}
